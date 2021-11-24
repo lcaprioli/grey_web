1e20ab2cc852192eb28ed55a9c535faefe5cd51f
luizcaprioli <61696032+luizcaprioli@users.noreply.github.com>
2021-11-23 23:57:04 -0300

    colum stable

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -18,9 +18,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "c26e91ecaa89c76b2f659f7db6355425",
-"/": "c26e91ecaa89c76b2f659f7db6355425",
-"main.dart.js": "24d634ea091272a0b6cee3e35d1a2c36",
+"index.html": "6577f2de2d0a5738572aebaff6580d26",
+"/": "6577f2de2d0a5738572aebaff6580d26",
+"main.dart.js": "c4fbe9f0553681e5aa4ee791ef69c0a6",
 "manifest.json": "9a8518e9c61a76e512034deeadfe7991",
 "version.json": "502fe4c57dae28431480c26cef2e7c65"
 };
